a3a06047fd3c8b7ee67b4eceb612ac3fda9eb09b
zubair <zubairzia0@gmail.com>
2018-06-06 23:14:02 -0400

    Creating Repo for snippetbox app

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,20 +2,44 @@ package main
 
 import (
 	"net/http"
+	"strconv"
+	"fmt"
+	"html/template"
+	"log"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	// Use r.URL.Path to check whether the request URL path exactly matches "/".
-   // If it doesn't, use the w.WriteHeader() method to send a 404 status code and
-   // the w.Write() method to write a "Not Found" response body. Importantly, we
-   // then return from the function so that the subsequent code is not executed.
-   if r.URL.Path != "/" {
-	   //w.WriteHeader(404)
-	   //w.Write([]byte("Not Found"))
-	   // Use the http.NotFound() function to send a 404 Not Found response.
-	   http.NotFound(w, r)
-	   return
-   }
 
-   w.Write([]byte("Hello from MZZ's Snippetbox"))
+func Home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
+	files := []string{
+		"./ui/html/base.html",
+		"./ui/html/home.page.html",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal server error", 500)
+	}
+}
+func ShowSnippet(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		http.NotFound(w, r)
+		return
+	}
+	fmt.Fprintf(w, "display a speicific snippet (ID %d)", id)
+}
+
+func NewSnippet(w http.ResponseWriter,r *http.Request) {
+	w.Write([]byte("Display the new snippet form"))
 }
--- cmd/web/main.go
+++ cmd/web/main.go
@@ -5,10 +5,16 @@ import (
 	"net/http"
 )
 
-
-func main () {
+func main() {
+	//mux is a router in go, it stores a maping between the URL patterns for app and the routers
+	//usually one server mux for one app containign all routes
+	// Use the http.NewServeMux() function to initialize a new serve mux. Then use
+	// the mux.HandleFunc() method to register our Home function as the handler for
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", Home)
+	mux.HandleFunc("/snippet", ShowSnippet)
+	mux.HandleFunc("/snippet/new", NewSnippet)
+
 
 	log.Println("Starting server on :4000")
 	err := http.ListenAndServe(":4000", mux)
